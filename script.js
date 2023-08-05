78ff7fedbacea96536301d8485aa4fce88c01ccc
Antonio Sertić <45394148+AntonioSertic23@users.noreply.github.com>
2023-08-05 16:22:11 +0200

    Account deletion and logout

--- script.js
+++ script.js
@@ -176,3 +176,24 @@ btnTransfer.addEventListener("click", function (e) {
   // Update UI
   updateUI(currentAccount);
 });
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
